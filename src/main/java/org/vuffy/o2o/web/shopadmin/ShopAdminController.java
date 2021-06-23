812c23a9146ba2aaa6ef8906887cf8b34c17ed09
vuffy <monkeyd.vuffy@gmail.com>
2021-06-23 20:18:55 +0800

    fix my IDEA

--- src/main/java/org/vuffy/o2o/web/shopadmin/ShopAdminController.java
+++ src/main/java/org/vuffy/o2o/web/shopadmin/ShopAdminController.java
@@ -8,35 +8,41 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * 主要用来解析路由并转发到相应的 html 中
  *
  * @author vuffy
- * @date 2021/5/20 9:53 下午
  * @version 1.0
+ * @date 2021/5/20 9:53 下午
  */
 @Controller
 @RequestMapping(
-    value = "shopadmin",
-    method = {RequestMethod.GET})
+        value = "shopadmin",
+        method = {RequestMethod.GET})
 public class ShopAdminController {
 
-  @RequestMapping(value = "/shopoperation")
-  public String shopOperation() {
-    // 转发至店铺注册/编辑页面
-    return "shop/shopoperation";
-  }
+    @RequestMapping(value = "/shopoperation")
+    public String shopOperation() {
+        // 转发至店铺注册/编辑页面
+        return "shop/shopoperation";
+    }
 
-  @RequestMapping(value = "/shoplist")
-  public String shopList() {
-    // 转发至店铺列表页面
-    return "shop/shoplist";
-  }
+    @RequestMapping(value = "/shoplist")
+    public String shopList() {
+        // 转发至店铺列表页面
+        return "shop/shoplist";
+    }
 
-  @RequestMapping(value = "/shopmanagement")
-  public String shopManagement() {
-    // 转发至店铺列表页面
-    return "shop/shopmanagement";
-  }
+    @RequestMapping(value = "/shopmanagement")
+    public String shopManagement() {
+        // 转发至店铺列表页面
+        return "shop/shopmanagement";
+    }
 
-  @RequestMapping(value = "/productcategorymanagement", method = RequestMethod.GET)
-  private String productCategoryManagement(){
-    return "shop/productcategorymanagement";
-  }
- }
+    @RequestMapping(value = "/productcategorymanagement", method = RequestMethod.GET)
+    private String productCategoryManagement() {
+        return "shop/productcategorymanagement";
+    }
+
+    @RequestMapping(value = "/productoperation")
+    public String productOperation() {
+        // 转发至商品添加/编辑页面
+        return "shop/productoperation";
+    }
+}
